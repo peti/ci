ec06496073fc38b0344a16e7eca2d0d5eeea8fea
Peter Simons <simons@cryp.to>
2013-04-24 14:16:15 +0200

    Move ci instructions for autoconf-archive, mini-httpd, and fastcgi library here.

---
+++ fastcgi.nix
@@ -0,0 +1,26 @@
+/* Build instructions for the continuous integration system Hydra. */
+
+{ fastcgiSrc ? { outPath = ../fastcgi; revCount = 0; gitTag = "dirty"; }
+}:
+
+let
+  pkgs = import <nixpkgs> { };
+  version = fastcgiSrc.gitTag;
+  versionSuffix = "";
+in
+rec {
+
+  tarball = pkgs.releaseTools.sourceTarball {
+    name = "fastcgi-tarball";
+    src = fastcgiSrc;
+    inherit version versionSuffix;
+  };
+
+  build = pkgs.lib.genAttrs [ "x86_64-linux" ] (system:
+    let pkgs = import <nixpkgs> { inherit system; }; in
+    pkgs.releaseTools.nixBuild {
+      name = "fastcgi";
+      src = tarball;
+      buildInputs = [ pkgs.boostHeaders ];
+    });
+}
---
+++ mini-httpd.nix
@@ -0,0 +1,41 @@
+/* Build instructions for the continuous integration system Hydra. */
+
+{ miniHttpdSrc ? { outPath = ../mini-httpd; revCount = 0; gitTag = "dirty"; }
+}:
+
+let
+  pkgs = import <nixpkgs> { };
+  version = miniHttpdSrc.gitTag;
+  versionSuffix = "";
+in
+rec {
+
+  tarball = pkgs.releaseTools.sourceTarball {
+    name = "mini-httpd-tarball";
+    src = miniHttpdSrc;
+    inherit version versionSuffix;
+    buildInputs = with pkgs; [
+      git perl asciidoc libxml2 asciidoc texinfo xmlto docbook2x
+      docbook_xsl docbook_xml_dtd_45 libxslt boostHeaders
+    ];
+    postUnpack = ''
+      cp -r ${pkgs.gnulib}/ gnulib/
+      chmod -R u+w gnulib
+      patchShebangs gnulib
+      ln -s ../gnulib $sourceRoot/gnulib
+    '';
+    distPhase = ''
+      make distcheck
+      mkdir $out/tarballs
+      mv -v mini-httpd-*.tar* $out/tarballs/
+    '';
+  };
+
+  build = pkgs.lib.genAttrs [ "x86_64-linux" ] (system:
+    let pkgs = import <nixpkgs> { inherit system; }; in
+    pkgs.releaseTools.nixBuild {
+      name = "mini-httpd";
+      src = tarball;
+      buildInputs = [ pkgs.boostHeaders ];
+    });
+}

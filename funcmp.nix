4b7954b6d0a2d119e1cde7b67bc47033f535b3b4
Peter Simons <simons@cryp.to>
2013-04-25 09:15:10 +0200

    funcmp.nix: add ci instructions for funcmp

---
+++ funcmp.nix
@@ -0,0 +1,22 @@
+/* Build instructions for the continuous integration system Hydra. */
+
+{ funcmpSrc ? { outPath = ../funcmp; revCount = 0; gitTag = "dirty"; }
+, supportedPlatforms ? [ "x86_64-linux" ]
+, supportedCompilers ? [ "ghc6104" "ghc6123" "ghc704" "ghc742" "ghc762" ]
+}:
+
+let
+  genAttrs = (import <nixpkgs> { }).lib.genAttrs;
+in
+rec {
+  funcmp = genAttrs supportedCompilers (ghcVer: genAttrs supportedPlatforms (system:
+    let
+      pkgs = import <nixpkgs> { inherit system; };
+      haskellPackages = pkgs.lib.getAttrFromPath ["haskellPackages_${ghcVer}"] pkgs;
+    in
+    haskellPackages.cabal.mkDerivation (self: {
+      pname = "funcmp";
+      src = funcmpSrc;
+      version = funcmpSrc.gitTag;
+    })));
+}

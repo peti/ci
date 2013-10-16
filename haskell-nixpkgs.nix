e2a280456baf7176b4d5f47b44bbcf5d46a5ab4b
Peter Simons <simons@cryp.to>
2013-10-16 17:11:58 +0200

    haskell-nixpkgs.nix: update Cabal

--- haskell-nixpkgs.nix
+++ haskell-nixpkgs.nix
@@ -80,8 +80,9 @@ mapHaskellTestOn {
   bytestringMmap = default;
   bytestringNums = default;
   bytestringTrie = default;
+  Cabal_1_14_0 = all;
   Cabal_1_16_0_3 = all;
-  Cabal_1_18_1 = allBut [ghc6104];
+  Cabal_1_18_1_1 = allBut [ghc6104];
   cabal2Ghci = default;
   cabal2nix = allBut [ghc6104];
   cabalDev = default;

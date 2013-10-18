800069043f3f8dbcb971a8ea22025e91690c8057
Peter Simons <simons@cryp.to>
2013-10-18 10:49:23 +0200

    haskell-nixpkgs.nix: build Cabal 1.14.x only with older compilers

--- haskell-nixpkgs.nix
+++ haskell-nixpkgs.nix
@@ -80,7 +80,7 @@ mapHaskellTestOn {
   bytestringMmap = default;
   bytestringNums = default;
   bytestringTrie = default;
-  Cabal_1_14_0 = all;
+  Cabal_1_14_0 = [ ghc6104 ghc6123 ghc704 ];
   Cabal_1_16_0_3 = all;
   Cabal_1_18_1_1 = allBut [ghc6104];
   cabal2Ghci = default;

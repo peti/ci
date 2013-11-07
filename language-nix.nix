6e3726d3261e63287a7bf6077b5560adb311f9f4
Peter Simons <simons@cryp.to>
2013-11-07 13:14:28 +0100

    language-nix.nix: add hydra build instructions for this package

---
+++ language-nix.nix
@@ -0,0 +1,34 @@
+/* Build instructions for the continuous integration system Hydra. */
+
+{ languageNixSrc ? { outPath = ../language-nix; revCount = 0; gitTag = "dirty"; }
+, supportedPlatforms ? [ "x86_64-linux" ]
+}:
+
+let
+  genAttrs = (import <nixpkgs> { }).lib.genAttrs;
+in
+{
+  languageNix = genAttrs ["ghc6123" "ghc704" "ghc742" "ghc763" "ghcHEAD"] (ghcVer: genAttrs supportedPlatforms (system:
+    let
+      pkgs = import <nixpkgs> { inherit system; };
+      haskellPackages = pkgs.lib.getAttrFromPath ["haskellPackages_${ghcVer}"] pkgs;
+      Cabal = haskellPackages.Cabal_1_18_1_2;
+      hackageDb = haskellPackages.hackageDb.override { inherit Cabal; };
+    in
+    haskellPackages.cabal.mkDerivation (self: {
+      pname = "language-nix";
+      src = languageNixSrc;
+      version = languageNixSrc.gitTag;
+      isLibrary = true;
+      isExecutable = true;
+      buildDepends = with haskellPackages; [ mtl QuickCheck transformers uuParsinglib parsec ];
+      testDepends = with haskellPackages; [ doctest hspec HUnit mtl QuickCheck transformers uuParsinglib parsec ];
+      meta = {
+        homepage = "https://github.com/peti/language-nix";
+        description = "Haskell AST and Parsers for the Nix language";
+        license = self.stdenv.lib.licenses.bsd3;
+        platforms = self.ghc.meta.platforms;
+      };
+    })
+  ));
+}

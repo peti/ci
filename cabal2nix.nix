943ad731eb4a17b5de6cd35434542e1a81349ae0
Peter Simons <simons@cryp.to>
2013-11-11 19:58:41 +0100

    cabal2nix.nix: override Cabal only on pre 7.x versions of GHC

--- cabal2nix.nix
+++ cabal2nix.nix
@@ -12,7 +12,7 @@ in
     let
       pkgs = import <nixpkgs> { inherit system; };
       haskellPackages = pkgs.lib.getAttrFromPath ["haskellPackages_${ghcVer}"] pkgs;
-      Cabal = haskellPackages.Cabal_1_18_1_2;
+      Cabal = if pkgs.lib.versionOlder haskellPackages.ghcPlain.version "7" then haskellPackages.Cabal_1_16_0_3 else null;
       hackageDb = haskellPackages.hackageDb.override { inherit Cabal; };
     in
     haskellPackages.cabal.mkDerivation (self: {

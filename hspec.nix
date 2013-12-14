f424356d477668e495d8acfcbbb35dc10df475cf
Peter Simons <simons@cryp.to>
2013-12-14 12:10:17 +0100

    hspec.nix: add continuous builds for hspec

---
+++ hspec.nix
@@ -0,0 +1,65 @@
+/* Build instructions for the continuous integration system Hydra. */
+
+{ hspecExpectationsSrc ? { outPath = ../hspec-expectations; revCount = 0; gitTag = "dirty"; }
+, hspecSrc ? { outPath = ../hspec; revCount = 0; gitTag = "dirty"; }
+, supportedPlatforms ? [ "x86_64-linux" ]
+}:
+
+let
+  genAttrs = (import <nixpkgs> { }).lib.genAttrs;
+
+  peti = "Peter Simons <simons@cryp.to>";
+  simon = "Simon Michael <sol@typeful.net>";
+in
+rec {
+  hspecExpectations = genAttrs ["ghc704" "ghc742" "ghc763"] (ghcVer: genAttrs supportedPlatforms (system:
+    let
+      pkgs = import <nixpkgs> { inherit system; };
+      haskellPackages = pkgs.lib.getAttrFromPath ["haskellPackages_${ghcVer}"] pkgs;
+      hspec = haskellPackages.hspec;
+    in
+    haskellPackages.cabal.mkDerivation (self: {
+      pname = "hspec-expectations";
+      src = hspecExpectationsSrc;
+      version = hspecExpectationsSrc.gitTag;
+      buildDepends = with haskellPackages; [ HUnit ];
+      testDepends = with haskellPackages; [ hspec HUnit markdownUnlit silently ];
+      meta = {
+        homepage = "https://github.com/sol/hspec-expectations#readme";
+        description = "Catchy combinators for HUnit";
+        license = self.stdenv.lib.licenses.mit;
+        platforms = self.ghc.meta.platforms;
+        maintainers = [simon peti];
+      };
+    })));
+
+  hspec = genAttrs ["ghc704" "ghc742" "ghc763"] (ghcVer: genAttrs supportedPlatforms (system:
+    let
+      pkgs = import <nixpkgs> { inherit system; };
+      haskellPackages = pkgs.lib.getAttrFromPath ["haskellPackages_${ghcVer}"] pkgs;
+      myHspecExpectations = pkgs.lib.getAttrFromPath [ghcVer system] hspecExpectations;
+    in
+    haskellPackages.cabal.mkDerivation (self: {
+      pname = "hspec";
+      src = hspecSrc;
+      version = hspecSrc.gitTag;
+      isLibrary = true;
+      isExecutable = true;
+      buildDepends = with haskellPackages; [
+        ansiTerminal deepseq filepath myHspecExpectations HUnit QuickCheck
+        quickcheckIo random setenv time transformers
+      ];
+      testDepends = with haskellPackages; [
+        ansiTerminal deepseq doctest filepath ghcPaths myHspecExpectations
+        hspecMeta HUnit QuickCheck quickcheckIo random setenv silently time
+        transformers
+      ];
+      meta = {
+        homepage = "http://hspec.github.com/";
+        description = "Behavior-Driven Development for Haskell";
+        license = self.stdenv.lib.licenses.bsd3;
+        platforms = self.ghc.meta.platforms;
+        maintainers = [simon peti];
+      };
+    })));
+}

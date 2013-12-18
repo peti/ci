8c23e131a647280337e51fd5fe3612c7fd517cd1
Peter Simons <simons@cryp.to>
2013-12-18 18:22:28 +0100

    hspec.nix: build and test hspec with use 'hspec-expectations' from Hackage

--- hspec.nix
+++ hspec.nix
@@ -1,7 +1,6 @@
 /* Build instructions for the continuous integration system Hydra. */
 
-{ hspecExpectationsSrc ? { outPath = ../hspec-expectations; revCount = 0; gitTag = "dirty"; }
-, hspecSrc ? { outPath = ../hspec; revCount = 0; gitTag = "dirty"; }
+{ hspecSrc ? { outPath = ../hspec; revCount = 0; gitTag = "dirty"; }
 , supportedPlatforms ? [ "x86_64-linux" ]
 }:
 
@@ -10,52 +9,28 @@ let
 
   peti = "Peter Simons <simons@cryp.to>";
   simon = "Simon Hengel <sol@typeful.net>";
-
-  hspecExpectations = doCheck: genAttrs ["ghc704" "ghc742" "ghc763"] (ghcVer: genAttrs supportedPlatforms (system:
+in
+{
+  hspec = genAttrs ["ghc704" "ghc742" "ghc763"] (ghcVer: genAttrs supportedPlatforms (system:
     let
       pkgs = import <nixpkgs> { inherit system; };
       haskellPackages = pkgs.lib.getAttrFromPath ["haskellPackages_${ghcVer}"] pkgs;
-      myHspec = pkgs.lib.getAttrFromPath [ghcVer system] (hspec false);
     in
     haskellPackages.cabal.mkDerivation (self: {
-      pname = pkgs.lib.optionalString (!doCheck) "untested-" + "hspec-expectations";
-      src = hspecExpectationsSrc;
-      version = hspecExpectationsSrc.gitTag;
-      buildDepends = with haskellPackages; [ HUnit ];
-      testDepends = with haskellPackages; [ myHspec HUnit markdownUnlit silently ];
-      inherit doCheck;
-      meta = {
-        homepage = "https://github.com/sol/hspec-expectations#readme";
-        description = "Catchy combinators for HUnit";
-        license = self.stdenv.lib.licenses.mit;
-        platforms = self.ghc.meta.platforms;
-        maintainers = [simon peti];
-      };
-    })));
-
-  hspec = doCheck: genAttrs ["ghc704" "ghc742" "ghc763"] (ghcVer: genAttrs supportedPlatforms (system:
-    let
-      pkgs = import <nixpkgs> { inherit system; };
-      haskellPackages = pkgs.lib.getAttrFromPath ["haskellPackages_${ghcVer}"] pkgs;
-      myHspecExpectations = pkgs.lib.getAttrFromPath [ghcVer system] (hspecExpectations doCheck);
-      myHspecMeta = haskellPackages.hspecMeta.override { hspecExpectations = myHspecExpectations; };
-    in
-    haskellPackages.cabal.mkDerivation (self: {
-      pname = pkgs.lib.optionalString (!doCheck) "untested-" + "hspec";
+      pname = "hspec";
       src = hspecSrc;
       version = hspecSrc.gitTag;
       isLibrary = true;
       isExecutable = true;
       buildDepends = with haskellPackages; [
-        ansiTerminal deepseq filepath myHspecExpectations HUnit QuickCheck
+        ansiTerminal deepseq filepath hspecExpectations HUnit QuickCheck
         quickcheckIo random setenv time transformers
       ];
       testDepends = with haskellPackages; [
-        ansiTerminal deepseq doctest filepath ghcPaths myHspecExpectations
-        myHspecMeta HUnit QuickCheck quickcheckIo random setenv silently time
+        ansiTerminal deepseq doctest filepath ghcPaths hspecExpectations
+        hspecMeta HUnit QuickCheck quickcheckIo random setenv silently time
         transformers
       ];
-      inherit doCheck;
       meta = {
         homepage = "http://hspec.github.com/";
         description = "Behavior-Driven Development for Haskell";
@@ -63,9 +38,6 @@ let
         platforms = self.ghc.meta.platforms;
         maintainers = [simon peti];
       };
-    })));
-in
-{
-  hspecExpectations = hspecExpectations true;
-  hspec = hspec true;
+    })
+  ));
 }

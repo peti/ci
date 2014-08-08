52f4b0507170e4d507688522c9445622660d7952
Peter Simons <simons@cryp.to>
2014-08-08 12:16:13 +0200

    hsyslog.nix: add the new doctest dependency
    Plus some Cabal magic to make it work older compilers.

--- hsyslog.nix
+++ hsyslog.nix
@@ -13,11 +13,16 @@ in
     let
       pkgs = import <nixpkgs> { inherit system; };
       haskellPackages = pkgs.lib.getAttrFromPath ["haskellPackages_${ghcVer}"] pkgs;
+      Cabal = if pkgs.lib.versionOlder haskellPackages.ghc.version "7" then haskellPackages.Cabal_1_16_0_3 else null;
+      cabal = haskellPackages.cabal.override { inherit Cabal; };
     in
-    haskellPackages.cabal.mkDerivation (self: {
+    cabal.mkDerivation (self: {
       pname = "hsyslog";
       src = hsyslogSrc;
       version = hsyslogSrc.gitTag;
+      testDepends = with haskellPackages; [doctest];
+      doCheck = pkgs.lib.versionOlder "7" haskellPackages.ghc.version;
+      # noHaddock = pkgs.lib.versionOlder haskellPackages.ghc.version "6.12";
       meta = {
         homepage = "http://github.com/peti/hsyslog";
         description = "FFI interface to syslog(3) from POSIX.1-2001.";

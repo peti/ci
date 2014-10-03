010aa08d8762e4d1af10f00991c537d4f42a38a8
Peter Simons <simons@cryp.to>
2014-10-03 22:10:01 +0200

    haskell-nixpkgs.nix: give up GHC 6.x builds for cabal2nix

--- haskell-nixpkgs.nix
+++ haskell-nixpkgs.nix
@@ -77,7 +77,7 @@ mapHaskellTestOn {
   Cabal_1_16_0_3 = [ ghc6104 ghc6123 ghc704 ghc722 ghc742 ghc763 ];
   Cabal_1_18_1_3 = [ ghc704 ghc722 ghc742 ghc763 ghc783 ];
   Cabal_1_20_0_2 = [ ghc704 ghc722 ghc742 ghc763 ghc783 ];
-  cabal2nix = allBut [ghc6104];
+  cabal2nix = allBut [ghc6104 ghc6123];
   cabalInstall_1_20_0_3 = allBut [ ghc6104 ghc6123 ghcHEAD ];
   cabalInstall = allBut [ ghcHEAD ];
   caseInsensitive = all;

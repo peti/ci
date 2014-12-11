31c9ff6ba5439be49e52317ebcfb8a4cf58ad5e6
Peter Simons <simons@cryp.to>
2014-12-11 13:35:09 +0100

    hackage-db.nix: add Hydra CI

---
+++ hackage-db.nix
@@ -0,0 +1,23 @@
+/* Build instructions for the continuous integration system Hydra. */
+
+{ hackageDbSrc ? { outPath = ../hackage-db; revCount = 0; gitTag = "dirty"; }
+, supportedPlatforms ? [ "x86_64-linux" ]
+, supportedCompilers ? [ "ghc6104" "ghc6123" "ghc704" "ghc722" "ghc742" "ghc763" "ghc783" /*"ghcHEAD"*/ ]
+}:
+
+let
+  genAttrs = (import <nixpkgs> { }).lib.genAttrs;
+in
+rec {
+  hackageDb = genAttrs supportedCompilers (ghcVer: genAttrs supportedPlatforms (system:
+    let
+      pkgs = import <nixpkgs> { inherit system; };
+      haskellPackages = pkgs.lib.getAttrFromPath ["haskellPackages_${ghcVer}"] pkgs;
+    in
+    haskellPackages.cabal.mkDerivation (self: {
+      pname = "hackageDb";
+      src = hackageDbSrc;
+      buildDepends = with haskellPackages; [ Cabal filepath tar utf8String ];
+      version = hackageDbSrc.gitTag;
+    })));
+}

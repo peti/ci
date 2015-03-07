8ccc1e733657bf282f19ca992a57eca8a71c92aa
Peter Simons <simons@cryp.to>
2015-03-07 15:02:13 +0100

    hsyslog-pkg.nix: re-generate

--- hsyslog-pkg.nix
+++ hsyslog-pkg.nix
@@ -1,14 +1,13 @@
-{ mkDerivation, base, doctest, fetchgit, stdenv }:
+{ mkDerivation, base, fetchgit, stdenv }:
 mkDerivation {
   pname = "hsyslog";
-  version = "2.0";
+  version = "3";
   src = fetchgit {
     url = "git://github.com/peti/hsyslog.git";
-    sha256 = "ef82cd0697206d2616195aeeec823a02ceee2373a644e4d8694b448c0bf96b0c";
-    rev = "ae374d72f9cf65eea308a419587893db4e5c4505";
+    sha256 = "9c69bd28ac36a35c00e9c7415710f5bedab5ba980f697f4d5272961991fb50d7";
+    rev = "cf6be8a612995502853b187cc9cdc83c835cce30";
   };
   buildDepends = [ base ];
-  testDepends = [ base doctest ];
   homepage = "http://github.com/peti/hsyslog";
   description = "FFI interface to syslog(3) from POSIX.1-2001";
   license = stdenv.lib.licenses.bsd3;

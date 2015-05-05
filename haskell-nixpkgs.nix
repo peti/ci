c4f37bfff66fbe8df2cb8299452530f2fe265c03
Peter Simons <simons@cryp.to>
2015-05-05 21:28:28 +0200

    haskell-nixpkgs.nix: ghc 6.x can't build cabal-install

--- haskell-nixpkgs.nix
+++ haskell-nixpkgs.nix
@@ -62,7 +62,7 @@ pkgs.lib.optionalAttrs buildAllNGPackages (mapTestOn {
   Cabal_1_20_0_3 = [ghc704 ghc722 ghc742 ghc763 ghc784];
   Cabal_1_22_1_0 = [ghc722 ghc742 ghc763 ghc784 ghc7101 ghcHEAD];
   cabal2nix = default;
-  cabal-install = all;
+  cabal-install = allBut [ghc6123];
   case-insensitive = all;
   cpphs = all;
   data-memocombinators = all;

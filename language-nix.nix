cdc92c61a8f8a1fc4bd0636c687bb79279ff5535
Peter Simons <simons@cryp.to>
2015-09-10 12:12:44 +0200

    language-nix.nix: enable builds with older compilers again

--- language-nix.nix
+++ language-nix.nix
@@ -7,7 +7,6 @@
   language-nix = import ./generate-haskell-build.nix {
     gitSource = languageNixSrc;
     expressionPath = ./language-nix-pkg.nix;
-    supportedCompilers = ["ghc7102"];
   };
 
 }

145eb5523397982946cea09da48c2345b98eed32
Peter Simons <simons@cryp.to>
2015-09-10 12:59:08 +0200

    language-nix-pkg.nix: update auto-generated build instructions

--- language-nix-pkg.nix
+++ language-nix-pkg.nix
@@ -1,27 +1,30 @@
 { mkDerivation, base, containers, doctest, fetchgit, hspec, HUnit
 , mtl, parsec, QuickCheck, stdenv, transformers
+, transformers-compat
 }:
 mkDerivation {
   pname = "language-nix";
-  version = "1.0";
+  version = "1.0.1";
   src = fetchgit {
     url = "git://github.com/peti/language-nix.git";
-    sha256 = "322645314fd0b8bc3966408748157775d195cb01884657ab4f1ffe352060e3d8";
-    rev = "e1a469b901584f9cc466da16bb00d77d35b69650";
+    sha256 = "d410efa889272956e6595cc0eb37792df2a197724562224f11e846916a02e9bd";
+    rev = "e0641777d1e7292429c5c152e8370d2b2dab919b";
   };
   isLibrary = true;
   isExecutable = true;
   libraryHaskellDepends = [
     base containers mtl parsec QuickCheck transformers
+    transformers-compat
   ];
   executableHaskellDepends = [
     base containers mtl parsec QuickCheck transformers
+    transformers-compat
   ];
   testHaskellDepends = [
     base containers doctest hspec HUnit mtl parsec QuickCheck
-    transformers
+    transformers transformers-compat
   ];
   homepage = "https://github.com/peti/language-nix";
-  description = "Haskell AST and Parsers for the Nix language";
+  description = "Data types and useful functions to represent and manipulate the Nix language";
   license = stdenv.lib.licenses.bsd3;
 }

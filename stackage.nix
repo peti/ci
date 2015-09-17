379b7ca13d71f14c85d1031c0d860484e3ab5570
Peter Simons <simons@cryp.to>
2015-09-17 14:26:33 +0200

    stackage.nix: build lts 3.x only

--- stackage.nix
+++ stackage.nix
@@ -15,7 +15,7 @@ mapTestOn {
   # haskell.packages.lts-0_4 = packagePlatforms pkgs.haskell.packages.lts-0_4;
   # haskell.packages.lts-0_5 = packagePlatforms pkgs.haskell.packages.lts-0_5;
   # haskell.packages.lts-0_6 = packagePlatforms pkgs.haskell.packages.lts-0_6;
-  haskell.packages.lts-0_7 = packagePlatforms pkgs.haskell.packages.lts-0_7;
+  # haskell.packages.lts-0_7 = packagePlatforms pkgs.haskell.packages.lts-0_7;
   # haskell.packages.lts-1_0 = packagePlatforms pkgs.haskell.packages.lts-1_0;
   # haskell.packages.lts-1_1 = packagePlatforms pkgs.haskell.packages.lts-1_1;
   # haskell.packages.lts-1_2 = packagePlatforms pkgs.haskell.packages.lts-1_2;
@@ -29,7 +29,7 @@ mapTestOn {
   # haskell.packages.lts-1_12 = packagePlatforms pkgs.haskell.packages.lts-1_12;
   # haskell.packages.lts-1_13 = packagePlatforms pkgs.haskell.packages.lts-1_13;
   # haskell.packages.lts-1_14 = packagePlatforms pkgs.haskell.packages.lts-1_14;
-  haskell.packages.lts-1_15 = packagePlatforms pkgs.haskell.packages.lts-1_15;
+  # haskell.packages.lts-1_15 = packagePlatforms pkgs.haskell.packages.lts-1_15;
   # haskell.packages.lts-2_0 = packagePlatforms pkgs.haskell.packages.lts-2_0;
   # haskell.packages.lts-2_1 = packagePlatforms pkgs.haskell.packages.lts-2_1;
   # haskell.packages.lts-2_2 = packagePlatforms pkgs.haskell.packages.lts-2_2;
@@ -52,12 +52,12 @@ mapTestOn {
   # haskell.packages.lts-2_19 = packagePlatforms pkgs.haskell.packages.lts-2_19;
   # haskell.packages.lts-2_20 = packagePlatforms pkgs.haskell.packages.lts-2_20;
   # haskell.packages.lts-2_21 = packagePlatforms pkgs.haskell.packages.lts-2_21;
-  haskell.packages.lts-2_22 = packagePlatforms pkgs.haskell.packages.lts-2_22;
-  # haskell.packages.lts-3_0 = packagePlatforms pkgs.haskell.packages.lts-3_0;
-  # haskell.packages.lts-3_1 = packagePlatforms pkgs.haskell.packages.lts-3_1;
-  # haskell.packages.lts-3_2 = packagePlatforms pkgs.haskell.packages.lts-3_2;
-  # haskell.packages.lts-3_3 = packagePlatforms pkgs.haskell.packages.lts-3_3;
-  # haskell.packages.lts-3_4 = packagePlatforms pkgs.haskell.packages.lts-3_4;
+  # haskell.packages.lts-2_22 = packagePlatforms pkgs.haskell.packages.lts-2_22;
+  haskell.packages.lts-3_0 = packagePlatforms pkgs.haskell.packages.lts-3_0;
+  haskell.packages.lts-3_1 = packagePlatforms pkgs.haskell.packages.lts-3_1;
+  haskell.packages.lts-3_2 = packagePlatforms pkgs.haskell.packages.lts-3_2;
+  haskell.packages.lts-3_3 = packagePlatforms pkgs.haskell.packages.lts-3_3;
+  haskell.packages.lts-3_4 = packagePlatforms pkgs.haskell.packages.lts-3_4;
   haskell.packages.lts-3_5 = packagePlatforms pkgs.haskell.packages.lts-3_5;
 
 }

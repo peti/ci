c2026fdb0b6d482adc2baeca76047ab7b940102b
Peter Simons <simons@cryp.to>
2015-09-19 00:01:10 +0200

    language-nix-pkg.nix: update build expression

--- language-nix-pkg.nix
+++ language-nix-pkg.nix
@@ -1,21 +1,20 @@
-{ mkDerivation, base, deepseq, doctest, fetchgit, lens, pretty
-, prettyclass, QuickCheck, regex-posix, stdenv, ghc
+{ mkDerivation, base, Cabal, deepseq, doctest, fetchgit, lens
+, pretty, QuickCheck, stdenv
 }:
-
 mkDerivation {
   pname = "language-nix";
-  version = "2";
+  version = "2.1";
   src = fetchgit {
     url = "git://github.com/peti/language-nix.git";
-    sha256 = "33e46fe4acf182487386b06c0eaf7076c1a77ceb1d9111e804a28424e7c302c1";
-    rev = "3a287152877cf10f3bb052b5ccc2906357f02f01";
+    sha256 = "5491f66b6c8a5abcc572ad1791a7f5a5352634ff994727fe1a077fcf5f7a0439";
+    rev = "6631ad24127c756e8dfce199091cee81ace4f9a9";
   };
   libraryHaskellDepends = [
-    base deepseq lens pretty regex-posix
-  ] ++ stdenv.lib.optional (stdenv.lib.versionOlder ghc.version "7.10") prettyclass;
+    base Cabal deepseq lens pretty QuickCheck
+  ];
   testHaskellDepends = [
-    base deepseq doctest lens pretty QuickCheck regex-posix
-  ] ++ stdenv.lib.optional (stdenv.lib.versionOlder ghc.version "7.10") prettyclass;
+    base Cabal deepseq doctest lens pretty QuickCheck
+  ];
   homepage = "https://github.com/peti/language-nix#readme";
   description = "Data types and useful functions to represent and manipulate the Nix language";
   license = stdenv.lib.licenses.bsd3;

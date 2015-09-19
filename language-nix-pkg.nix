981b46609ca4c29a7f6285af929972d60722513f
Peter Simons <simons@cryp.to>
2015-09-19 19:35:53 +0200

    language-nix: re-generate build instructions

--- language-nix-pkg.nix
+++ language-nix-pkg.nix
@@ -1,19 +1,19 @@
-{ mkDerivation, base, Cabal, deepseq, doctest, fetchgit, lens
-, pretty, QuickCheck, stdenv
+{ mkDerivation, base, base-compat, Cabal, deepseq, doctest
+, fetchgit, lens, pretty, QuickCheck, stdenv
 }:
 mkDerivation {
   pname = "language-nix";
   version = "2.1";
   src = fetchgit {
     url = "git://github.com/peti/language-nix.git";
-    sha256 = "5491f66b6c8a5abcc572ad1791a7f5a5352634ff994727fe1a077fcf5f7a0439";
-    rev = "6631ad24127c756e8dfce199091cee81ace4f9a9";
+    sha256 = "cd76e0a0ee952cd42ade15020516f414336811ccc60ce3f1e9672c4fab29bae4";
+    rev = "77b675a02786b37611476fbb7ef29ad064f4fa3c";
   };
   libraryHaskellDepends = [
-    base Cabal deepseq lens pretty QuickCheck
+    base base-compat Cabal deepseq lens pretty QuickCheck
   ];
   testHaskellDepends = [
-    base Cabal deepseq doctest lens pretty QuickCheck
+    base base-compat Cabal deepseq doctest lens pretty QuickCheck
   ];
   homepage = "https://github.com/peti/language-nix#readme";
   description = "Data types and useful functions to represent and manipulate the Nix language";

66c932f46651ebf03c5066146343d142d3c44c1f
Peter Simons <simons@cryp.to>
2015-11-17 11:14:36 +0100

    haskell-nixpkgs.nix: update to LTS Haskell 3.14

--- haskell-nixpkgs.nix
+++ haskell-nixpkgs.nix
@@ -110,7 +110,8 @@ pkgs.lib.optionalAttrs buildAllNGPackages (mapTestOn {
   # haskell.packages.lts-3_10 = packagePlatforms pkgs.haskell.packages.lts-3_10;
   # haskell.packages.lts-3_11 = packagePlatforms pkgs.haskell.packages.lts-3_11;
   # haskell.packages.lts-3_12 = packagePlatforms pkgs.haskell.packages.lts-3_12;
-  haskell.packages.lts-3_13 = packagePlatforms pkgs.haskell.packages.lts-3_13;
+  # haskell.packages.lts-3_13 = packagePlatforms pkgs.haskell.packages.lts-3_13;
+  haskell.packages.lts-3_14 = packagePlatforms pkgs.haskell.packages.lts-3_14;
 
 })
 // mapHaskellTestOn {

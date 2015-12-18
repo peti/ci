753cfbd75e7cff218f857a33af07c513704d0303
Peter Simons <simons@cryp.to>
2015-12-18 11:18:51 +0100

    hsyslog-pkg.nix: update build instructions for new version

--- hsyslog-pkg.nix
+++ hsyslog-pkg.nix
@@ -1,13 +1,14 @@
-{ mkDerivation, base, fetchgit, stdenv }:
+{ mkDerivation, base, bytestring, fetchgit, QuickCheck, stdenv }:
 mkDerivation {
   pname = "hsyslog";
   version = "3";
   src = fetchgit {
     url = "git://github.com/peti/hsyslog.git";
-    sha256 = "9c69bd28ac36a35c00e9c7415710f5bedab5ba980f697f4d5272961991fb50d7";
-    rev = "cf6be8a612995502853b187cc9cdc83c835cce30";
+    sha256 = "29174424d0a2d8f4a313950aa6e2c20e057fb1c5aabde79ffa60add7998cdeaa";
+    rev = "f2c4ee8da880a5e885463da769168413f6972291";
   };
-  libraryHaskellDepends = [ base ];
+  libraryHaskellDepends = [ base bytestring ];
+  testHaskellDepends = [ base bytestring QuickCheck ];
   homepage = "http://github.com/peti/hsyslog";
   description = "FFI interface to syslog(3) from POSIX.1-2001";
   license = stdenv.lib.licenses.bsd3;

1ab648a3e7daf468aa6ff787fba9a0d1e2cf6fdb
Peter Simons <simons@cryp.to>
2015-12-28 13:27:29 +0100

    haskell-nixpkgs.nix: update to LTS Haskell 3.20.

--- haskell-nixpkgs.nix
+++ haskell-nixpkgs.nix
@@ -116,7 +116,8 @@ pkgs.lib.optionalAttrs buildAllNGPackages (mapTestOn {
   # haskell.packages.lts-3_16 = packagePlatforms pkgs.haskell.packages.lts-3_16;
   # haskell.packages.lts-3_17 = packagePlatforms pkgs.haskell.packages.lts-3_17;
   # haskell.packages.lts-3_18 = packagePlatforms pkgs.haskell.packages.lts-3_18;
-  haskell.packages.lts-3_19 = packagePlatforms pkgs.haskell.packages.lts-3_19;
+  # haskell.packages.lts-3_19 = packagePlatforms pkgs.haskell.packages.lts-3_19;
+  haskell.packages.lts-3_20 = packagePlatforms pkgs.haskell.packages.lts-3_20;
 
 })
 // mapHaskellTestOn {

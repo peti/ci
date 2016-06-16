c240004203a6532681d87eb69a290974c08699c7
Peter Simons <simons@cryp.to>
2016-06-16 14:23:13 +0200

    clean out obsolete builds

--- hydra.nix
+++
@@ -1,6 +0,0 @@
-/* Build instructions for the continuous integration system Hydra. */
-
-{ hydraSrc ? { outPath = ../hydra; revCount = 0; gitTag = "dirty"; }
-}:
-
-builtins.removeAttrs (import "${hydraSrc}/release.nix" { inherit hydraSrc; }) ["tests"]

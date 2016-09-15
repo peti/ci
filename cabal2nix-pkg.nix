ed1563470e34b4958b87778381e5eb96dcc0323d
Peter Simons <simons@cryp.to>
2016-09-15 19:18:13 +0200

    cabal2nix: update build instructions

--- cabal2nix-pkg.nix
+++ cabal2nix-pkg.nix
@@ -2,16 +2,15 @@
 , containers, deepseq, directory, distribution-nixpkgs, fetchgit
 , filepath, hackage-db, language-nix, lens, monad-par
 , monad-par-extras, mtl, optparse-applicative, pretty, process, SHA
-, split, stackage-types, stdenv, text, time, transformers
-, utf8-string, yaml
+, split, stdenv, text, time, transformers, utf8-string, yaml
 }:
 mkDerivation {
   pname = "cabal2nix";
-  version = "20160613";
+  version = "2.0.2";
   src = fetchgit {
     url = "git://github.com/NixOS/cabal2nix.git";
-    sha256 = "0bp1394vgfws2z6j8qnicvj1s6g2dx0snhqzlaclyqkaq256hq06";
-    rev = "1b9ba52ce78a5eba8bb68d12be5fa9c631e863c8";
+    sha256 = "0hk0lzhx60ljcxykis14vccv0ll6rf5xfhhpjj3sfcqkc6krir0g";
+    rev = "5889772b980df0404f3bb6db698814f606d443e6";
   };
   isLibrary = true;
   isExecutable = true;
@@ -25,8 +24,7 @@ mkDerivation {
     aeson ansi-wl-pprint base bytestring Cabal containers deepseq
     directory distribution-nixpkgs filepath hackage-db language-nix
     lens monad-par monad-par-extras mtl optparse-applicative pretty
-    process SHA split stackage-types text time transformers utf8-string
-    yaml
+    process SHA split text time transformers utf8-string yaml
   ];
   homepage = "https://github.com/nixos/cabal2nix#readme";
   description = "Convert Cabal files into Nix build instructions";

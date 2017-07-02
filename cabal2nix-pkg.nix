f8d5c34bafc76694ecdb7756c14515da37f7f182
Peter Simons <simons@cryp.to>
2017-07-02 12:25:33 +0200

    cabal2nix-pkg.nix: re-generate

--- cabal2nix-pkg.nix
+++ cabal2nix-pkg.nix
@@ -1,20 +1,21 @@
 { mkDerivation, aeson, ansi-wl-pprint, base, bytestring, Cabal
-, containers, deepseq, directory, distribution-nixpkgs, doctest
-, fetchgit, filepath, hackage-db, hopenssl, language-nix, lens
-, monad-par, monad-par-extras, mtl, optparse-applicative, pretty
-, process, split, stdenv, text, time, transformers, utf8-string
-, yaml
+, cabal-doctest, containers, deepseq, directory
+, distribution-nixpkgs, doctest, fetchgit, filepath, hackage-db
+, hopenssl, language-nix, lens, monad-par, monad-par-extras, mtl
+, optparse-applicative, pretty, process, split, stdenv, text, time
+, transformers, utf8-string, yaml
 }:
 mkDerivation {
   pname = "cabal2nix";
   version = "2.3";
   src = fetchgit {
     url = "git://github.com/NixOS/cabal2nix.git";
-    sha256 = "1hi876l6skx0wsdcj194zy8adhgi0m2pz0k0dbn4z4qxvik7c57v";
-    rev = "5c18fb530ced48f35549543943f8e0c8ddaf5686";
+    sha256 = "1ddnjgvndl6mn2q1y6a4rdd5i21d26nhnrwpvp1hiny8z846n48h";
+    rev = "4ff71879d053fd07da58ceeef25418e8ef86af4f";
   };
   isLibrary = true;
   isExecutable = true;
+  setupHaskellDepends = [ base Cabal cabal-doctest ];
   libraryHaskellDepends = [
     aeson ansi-wl-pprint base bytestring Cabal containers deepseq
     directory distribution-nixpkgs filepath hackage-db hopenssl

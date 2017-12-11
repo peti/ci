467cc7000fc641f20b5b034ace9482e22c85892d
Peter Simons <simons@cryp.to>
2017-12-11 15:18:22 +0100

    cabal2nix-pkg.nix: re-generate

--- cabal2nix-pkg.nix
+++ cabal2nix-pkg.nix
@@ -1,30 +1,30 @@
 { mkDerivation, aeson, ansi-wl-pprint, base, bytestring, Cabal
 , cabal-doctest, containers, deepseq, directory
 , distribution-nixpkgs, doctest, fetchgit, filepath, hackage-db
-, hopenssl, language-nix, lens, monad-par, monad-par-extras, mtl
-, optparse-applicative, pretty, process, split, stdenv, text, time
-, transformers, utf8-string, yaml
+, hopenssl, hpack, language-nix, lens, monad-par, monad-par-extras
+, mtl, optparse-applicative, pretty, process, split, stdenv, text
+, time, transformers, utf8-string, yaml
 }:
 mkDerivation {
   pname = "cabal2nix";
-  version = "2.3";
+  version = "2.7";
   src = fetchgit {
     url = "git://github.com/NixOS/cabal2nix.git";
-    sha256 = "1ddnjgvndl6mn2q1y6a4rdd5i21d26nhnrwpvp1hiny8z846n48h";
-    rev = "4ff71879d053fd07da58ceeef25418e8ef86af4f";
+    sha256 = "0h7395vmh0fpypjbqn6yqn2ms71g13w1wlwhnhz85ljdlfdhjq85";
+    rev = "cba06639637a6764d78ab11bc0961a6815f699fd";
   };
   isLibrary = true;
   isExecutable = true;
   setupHaskellDepends = [ base Cabal cabal-doctest ];
   libraryHaskellDepends = [
     aeson ansi-wl-pprint base bytestring Cabal containers deepseq
-    directory distribution-nixpkgs filepath hackage-db hopenssl
+    directory distribution-nixpkgs filepath hackage-db hopenssl hpack
     language-nix lens optparse-applicative pretty process split text
-    transformers yaml
+    time transformers yaml
   ];
   executableHaskellDepends = [
     aeson ansi-wl-pprint base bytestring Cabal containers deepseq
-    directory distribution-nixpkgs filepath hackage-db hopenssl
+    directory distribution-nixpkgs filepath hackage-db hopenssl hpack
     language-nix lens monad-par monad-par-extras mtl
     optparse-applicative pretty process split text time transformers
     utf8-string yaml
@@ -32,8 +32,8 @@ mkDerivation {
   testHaskellDepends = [
     aeson ansi-wl-pprint base bytestring Cabal containers deepseq
     directory distribution-nixpkgs doctest filepath hackage-db hopenssl
-    language-nix lens optparse-applicative pretty process split text
-    transformers yaml
+    hpack language-nix lens optparse-applicative pretty process split
+    text time transformers yaml
   ];
   homepage = "https://github.com/nixos/cabal2nix#readme";
   description = "Convert Cabal files into Nix build instructions";
